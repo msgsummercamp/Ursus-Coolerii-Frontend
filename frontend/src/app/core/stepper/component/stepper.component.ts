3a2163c26588a3d31a130751781459fda8ece867
Silea-Horia <horia.silea@gmail.com>
2025-07-30 12:28:21 +0300

    nav works

--- frontend/src/app/core/stepper/component/stepper.component.ts
+++ frontend/src/app/core/stepper/component/stepper.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { TranslocoDirective } from '@jsverse/transloco';
+import { TestFormComponent } from '../../../test-form/test-form.component';
 
 @Component({
   selector: 'app-stepper',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    TranslocoDirective,
+    TestFormComponent,
+  ],
   templateUrl: './stepper.component.html',
-  styleUrl: './stepper.component.scss'
+  styleUrl: './stepper.component.scss',
 })
-export class StepperComponent {
-
-}
+export class StepperComponent {}
