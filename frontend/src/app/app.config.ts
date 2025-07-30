54d152e8166ff4a7a362944c836c8467ba9443cd
Bence Molnar <149154980+Benq2016@users.noreply.github.com>
2025-07-30 13:45:24 +0200

    passenger-details/47
    * initial passenger form created
    
    * app.config.ts refreshed
    
    * small modifications were made

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,20 +8,24 @@ import { errorInterceptor } from './shared/interceptors/error.interceptor';
 import { CookieService } from 'ngx-cookie-service';
 import { TranslocoHttpLoader } from './transloco-loader';
 import { provideTransloco } from '@jsverse/transloco';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
-    CookieService, provideHttpClient(), provideTransloco({
-        config: { 
-          availableLangs: ['en', 'ro'],
-          defaultLang: 'en',
-          reRenderOnLangChange: true,
-          prodMode: !isDevMode(),
-        },
-        loader: TranslocoHttpLoader
-      }),
+    provideNativeDateAdapter(),
+    CookieService,
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'ro'],
+        defaultLang: 'en',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
   ],
 };
