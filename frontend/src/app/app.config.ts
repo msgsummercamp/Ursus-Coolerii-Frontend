f09517cba28b4a2bba2bcbd64774e50f79e9bd47
Voina Maria-Alexia <149065478+AlexiaVoina@users.noreply.github.com>
2025-07-31 09:07:06 +0300

    added flight details form (#8)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,22 +2,21 @@ import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angul
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './shared/interceptors/auth.interceptor';
-import { errorInterceptor } from './shared/interceptors/error.interceptor';
+import { provideHttpClient } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { TranslocoHttpLoader } from './transloco-loader';
 import { provideTransloco } from '@jsverse/transloco';
-import { provideNativeDateAdapter } from '@angular/material/core';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
+    provideHttpClient(),
     provideNativeDateAdapter(),
     CookieService,
     provideHttpClient(),
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
     provideTransloco({
       config: {
         availableLangs: ['en', 'ro'],
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
+import {FlightDetailsFormComponent} from "./core/layout/flight-details-form/component/flight-details-form.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/form', pathMatch: 'full' },
