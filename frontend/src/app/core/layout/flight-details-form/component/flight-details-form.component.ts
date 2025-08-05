98a8267aaacdb68ff431075646a9917436b96f93
Silea Horia <146817894+Silea-Horia@users.noreply.github.com>
2025-08-05 10:36:25 +0300

    flight-details-validations/66 (#29)
    * added validations and translations for the flight details form
    
    * removed duplicates
    
    * fixed regex

--- frontend/src/app/core/layout/flight-details-form/component/flight-details-form.component.ts
+++ frontend/src/app/core/layout/flight-details-form/component/flight-details-form.component.ts
@@ -1,8 +1,17 @@
 import { Component, inject, Input, OnDestroy, OnInit, output, signal } from '@angular/core';
-import { TranslocoPipe } from '@jsverse/transloco';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AirportService } from '../service/airport.service';
+import { TranslocoPipe } from '@jsverse/transloco';
+
 import { NgForOf } from '@angular/common';
-import { MatFormField, MatHint, MatInput, MatLabel, MatSuffix } from '@angular/material/input';
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatInput,
+  MatLabel,
+  MatSuffix,
+} from '@angular/material/input';
 import { MatOption } from '@angular/material/core';
 import {
   MatDatepicker,
@@ -17,7 +26,6 @@ import {
 import { MatAutocomplete, MatAutocompleteTrigger } from '@angular/material/autocomplete';
 import { startWith, Subject, takeUntil } from 'rxjs';
 import { AirlineAttributes, AirlineService } from '../service/airline.service';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AirportAttributes } from '../../../../shared/types/types';
 import { FlightDetailsForm } from '../../../../shared/types/form.types';
 
@@ -43,6 +51,7 @@ import { FlightDetailsForm } from '../../../../shared/types/form.types';
     MatTimepickerInput,
     MatAutocomplete,
     MatAutocompleteTrigger,
+    MatError,
   ],
 })
 export class FlightDetailsFormComponent implements OnInit, OnDestroy {
@@ -100,7 +109,7 @@ export class FlightDetailsFormComponent implements OnInit, OnDestroy {
     if (airlineList) {
       airlineList.subscribe((data: AirlineAttributes[]) => {
         const seenNames = new Set<string>();
-        this.airlines = data.filter(a => {
+        this.airlines = data.filter((a) => {
           if (seenNames.has(a.name)) return false;
           seenNames.add(a.name);
           return true;
