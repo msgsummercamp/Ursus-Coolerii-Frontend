bded89f099228d8acdb619cd4a83e5d6a7f381d5
Silea-Horia <horia.silea@gmail.com>
2025-08-06 10:51:18 +0300

    works somewhat 2

--- frontend/src/app/core/loading-spinner/component/loading-spinner.component.ts
+++ frontend/src/app/core/loading-spinner/component/loading-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
@@ -7,12 +7,4 @@ import { MatProgressSpinner } from '@angular/material/progress-spinner';
   templateUrl: './loading-spinner.component.html',
   styleUrl: './loading-spinner.component.scss',
 })
-export class LoadingSpinnerComponent {
-  protected readonly hideSpinner = signal(true);
-
-  constructor() {
-    setTimeout(() => {
-      this.hideSpinner.set(false);
-    }, 300);
-  }
-}
+export class LoadingSpinnerComponent {}
