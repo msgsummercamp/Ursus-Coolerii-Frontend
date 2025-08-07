d38c64e119cb13dbf32cdd2798868f96193ee02d
Barra Dora <162601728+dorabarra@users.noreply.github.com>
2025-08-07 16:44:21 +0300

    Home page/93 (#46)
    * Added homepage and changed routing(and internationalization of course)
    
    * Added home button in toolbar and redirect to home via logo

---
+++ frontend/src/app/core/home-page/home-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import { MatCardActions } from '@angular/material/card';
+import { TranslocoPipe } from '@jsverse/transloco';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
+  imports: [MatButton, MatCardActions, TranslocoPipe],
+})
+export class HomePageComponent {
+  constructor(private router: Router) {}
+
+  goToForm() {
+    this.router.navigate(['/form']);
+  }
+}
