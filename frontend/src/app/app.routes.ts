49120abfa5f3709aca3fb0f4a11b0751713c6779
dorabarra <dorabarra2004@gmail.com>
2025-08-07 16:12:52 +0300

    Added homepage and changed routing(and internationalization of course)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,12 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/form', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./core/home-page/home-page.component').then((m) => m.HomePageComponent),
+  },
   {
     path: 'form',
     loadComponent: () =>
