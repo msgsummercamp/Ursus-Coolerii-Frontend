1f32b5a8bce14a40e7cbca37b94130243ff07fe2
LucaciGeorge <george.antoniu.lucaci@stud.ubbcluj.ro>
2025-08-11 13:24:29 +0300

    added i18n

--- frontend/src/app/core/pop-up-gdpr/pop-up-gdpr.component.ts
+++ frontend/src/app/core/pop-up-gdpr/pop-up-gdpr.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import { MatDialogActions, MatDialogClose, MatDialogContent } from '@angular/material/dialog';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-pop-up-gdpr',
-  imports: [MatDialogActions, MatDialogContent, ReactiveFormsModule, MatButton, MatDialogClose],
+  imports: [
+    MatDialogActions,
+    MatDialogContent,
+    ReactiveFormsModule,
+    MatButton,
+    MatDialogClose,
+    TranslocoPipe,
+  ],
   templateUrl: './pop-up-gdpr.component.html',
   styleUrl: './pop-up-gdpr.component.scss',
 })
