6cff56ceeea44de3ba8d5673c5b22cbddbb20b85
dorabarra <dorabarra2004@gmail.com>
2025-08-11 16:35:03 +0300

    case-details with mat-card

--- frontend/src/app/core/case-details/case-details.component.ts
+++ frontend/src/app/core/case-details/case-details.component.ts
@@ -3,12 +3,22 @@ import { ActivatedRoute } from '@angular/router';
 import { CaseFileService } from '../layout/services/case-file.service';
 import { TranslocoPipe } from '@jsverse/transloco';
 import { DatePipe, NgForOf, NgIf } from '@angular/common';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'app-case-details',
   templateUrl: './case-details.component.html',
   styleUrls: ['./case-details.component.scss'],
-  imports: [TranslocoPipe, DatePipe, NgIf, NgForOf],
+  imports: [
+    TranslocoPipe,
+    DatePipe,
+    NgIf,
+    NgForOf,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+  ],
 })
 export class CaseDetailsComponent implements OnInit {
   caseDetails: any;
