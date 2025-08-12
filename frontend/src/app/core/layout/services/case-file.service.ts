2f5ead5d19d8a573d60a5a761201a810e7beca5d
Silea-Horia <horia.silea@gmail.com>
2025-08-12 10:42:26 +0300

    removed logs

--- frontend/src/app/core/layout/services/case-file.service.ts
+++ frontend/src/app/core/layout/services/case-file.service.ts
@@ -11,7 +11,6 @@ export class CaseFileService {
   constructor(private http: HttpClient) {}
 
   public calculateReward(departureAirport: string, destinationAirport: string): void {
-    console.log(departureAirport);
     this.http
       .post<number>('http://localhost:8080/api/case-files/calculate-reward', {
         departureAirport: departureAirport,
@@ -19,7 +18,6 @@ export class CaseFileService {
       })
       .pipe(
         tap((value) => {
-          console.log(value);
           this.reward.set(value);
         })
       )
