8ff564af82df6e9897ec967d8daf494dd1716a44
Benq2016 <molnarbence2014@gmail.com>
2025-08-13 16:02:04 +0300

    homepage restyled (with Tudor)

--- frontend/src/app/core/home-page/home-page.component.ts
+++ frontend/src/app/core/home-page/home-page.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle,
+} from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { TranslocoDirective, TranslocoPipe } from '@jsverse/transloco';
 import { MatButton } from '@angular/material/button';
-import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
-  imports: [MatButton, TranslocoPipe],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatCardSubtitle,
+    MatIcon,
+    TranslocoPipe,
+    TranslocoDirective,
+    MatButton,
+  ],
 })
 export class HomePageComponent {
   constructor(private router: Router) {}
