eeba986471c7ff12157b8eb1830594b4d110f80c
Silea-Horia <horia.silea@gmail.com>
2025-08-14 13:57:03 +0300

    improved logic for delete confirmation

--- frontend/src/app/shared/components/delete-confirmation-dialog/delete-confirmation-dialog.component.ts
+++ frontend/src/app/shared/components/delete-confirmation-dialog/delete-confirmation-dialog.component.ts
@@ -1,22 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   MatDialogActions,
-  MatDialogClose,
   MatDialogContent,
+  MatDialogRef,
   MatDialogTitle,
 } from '@angular/material/dialog';
 import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-delete-confirmation-dialog',
-  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatButton, MatDialogClose],
+  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatButton],
   templateUrl: './delete-confirmation-dialog.component.html',
   styleUrl: './delete-confirmation-dialog.component.scss',
 })
 export class DeleteConfirmationDialogComponent {
-  @Output() confirm = new EventEmitter<void>();
+  constructor(public dialogRef: MatDialogRef<DeleteConfirmationDialogComponent>) {}
 
   protected handleConfirmation() {
-    this.confirm.emit();
+    this.dialogRef.close(true);
+  }
+
+  protected handleCancel() {
+    this.dialogRef.close(false);
   }
 }
